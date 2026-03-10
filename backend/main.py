8643f91e55e0def93cd4ac5e1e6d91820f5ee116
Kamil Labus <klabus03@gmail.com>
2026-03-10 15:56:21 +0100

    feat(backend): add CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.search import router as search_router
 from app.pipeline import router as pipeline_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(search_router)
 app.include_router(pipeline_router)
 
